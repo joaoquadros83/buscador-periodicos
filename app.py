4b419d0e4f42486ce8a97e9405f0ba3246d50996
joaoquadros83 <123119366+joaoquadros83@users.noreply.github.com>
2026-07-08 10:09:39 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-# 1. CONFIGURAÇÃO PREMIUM DA PÁGINA
+# 1. CONFIGURAÇÃO DA PÁGINA (Deve ser estritamente a primeira linha de código do Streamlit)
 st.set_page_config(
-    page_title="SciIndex | Portal de Inteligência Científica",
-    page_icon="🔬",
-    layout="wide",
-    initial_sidebar_state="expanded"
+    page_title="Portal de Inteligência Periódica",
+    page_icon="https://png.pngtree.com/png-vector/20190302/ourmid/pngtree-atom-science-icon-design-template-vector-isolated-png-image_746905.jpg",
+    layout="wide"
 )
 
 # 2. INJEÇÃO DE CSS AVANÇADO (Design de Alto Padrão e Ajuste de Botões)
