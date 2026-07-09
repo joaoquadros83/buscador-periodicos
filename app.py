45a3ba70413cc94653e4af50eb57aebcea32183e
joaoquadros83 <123119366+joaoquadros83@users.noreply.github.com>
2026-07-08 23:58:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-# Acessando dados estruturados
-user = st.secrets["banco_dados"]["joaoquadros"]
-password = st.secrets["banco_dados"]["Cv840383!"]
-# Forma correta de ler um segredo no seu código Python:
-usuario = st.secrets["joaoquadros"]
+# Forma simples e direta de ler
+user = st.secrets["usuario"]
+password = st.secrets["senha"]
 
 # 1. CONFIGURAÇÃO PREMIUM DA PÁGINA
 st.set_page_config(
