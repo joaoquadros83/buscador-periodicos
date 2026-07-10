05e09afa56aa3a0d95f1c7e1b90f2a43b6ee7591
joaoquadros83 <123119366+joaoquadros83@users.noreply.github.com>
2026-07-10 14:05:20 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (e) => {
-  // O Streamlit precisa buscar os dados em tempo real, 
-  // então apenas passamos a requisição adiante
-  e.respondWith(fetch(e.request));
-});
