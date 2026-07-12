0f94a350b8fed276d2a1a40e04a8d68df5b7636a
joaoquadros83 <123119366+joaoquadros83@users.noreply.github.com>
2026-07-11 22:02:23 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1077,6 +1077,10 @@ with tab_ia:
                 """
                 
                 modelos_tentar = [
+                    "gemini-2.5-flash",
+                    "gemini-2.0-flash",
+                    "gemini-3.5-flash",
+                    "gemini-flash-latest",
                     "gemini-1.5-flash",
                     "gemini-1.5-flash-latest",
                     "gemini-1.5-pro",
