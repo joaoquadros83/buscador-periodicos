7a87ac852a0295cf737a3d6c0e0ac12fb0e055ef
João Quadros <123119366+joaoquadros83@users.noreply.github.com>
2026-07-13 14:16:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -31,6 +31,11 @@ def inicializar_firebase():
         
     return firestore.client()
 
+# Inicializa o banco de dados
+db = inicializar_firebase()
+
+st.title("Buscador de Periódicos 📚")
+
 # --- EXEMPLOS DE USO DO FIRESTORE ---
 
 # 2. Criar ou Atualizar dados do usuário (Salvar histórico de busca)
